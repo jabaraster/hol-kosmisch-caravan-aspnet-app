ba5d57df1dc040f44e4c8921b225a84ba88f224f
Tomoyuki Kawano <kawatomo@city.co.jp>
2019-12-16 16:47:17 +0900

    ASP.NET Coreに移行した

---
+++ MyWebApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MyWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // ViewBag.Message = Session["message"]?.ToString() ?? "";
+            // return View();
+            string message = HttpContext.Session.GetString("message");
+            return View(new MyForm { Message = message });
+        }
+
+        [HttpPost]
+        public ActionResult Index(MyForm item)
+        {
+            // if (!string.IsNullOrWhiteSpace(item?.Message))
+            // {
+            //     Session["message"] = item.Message;
+            // }
+            // return RedirectToAction("Index");
+            HttpContext.Session.SetString("message", item.Message);
+            return RedirectToAction("Index");
+        }
+    }
+}
